47c37676485e0a031cb03204d0d3e4db15813d40
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-10-02 22:10:23 +0300

    scroll animation competed

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -322,8 +322,45 @@ window.onload = function () { //когда весь контент загруз�
 		} */
 	}
 	//</PARALAX>=================================
-	// обновление корзины. Добавляет и удаляет товары
+	//<SCROLL-ANIMATION>=================================
+	const animItems = document.querySelectorAll('._anim-items');
 
+	if (animItems.length > 0) {
+		window.addEventListener('scroll', animOnScroll);
+		function animOnScroll() {
+			for (let index = 0; index < animItems.length; index++) {
+				const animItem = animItems[index];
+				const animItemHeight = animItem.offsetHeight;
+				const animItemOffset = offset(animItem).top;
+				const animStart = 4;
+
+				let animItemPoint = window.innerHeight - animItemHeight / animStart;
+				if (animItemHeight > window.innerHeight) {
+					animItemPoint = window.innerHeight - window.innerHeight / animStart;
+				}
+				if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+					animItem.classList.add('_active');
+				} else {
+					if (!animItem.classList.contains('_anim-no-hide')) {
+						animItem.classList.remove('_active');
+					}
+				}
+				if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				}
+			}
+		}
+		function offset(el) {
+			const rect = el.getBoundingClientRect(),
+				scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+				scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+		}
+
+		setTimeout(() => {
+			animOnScroll();
+		}, 300);
+	}
+	//</SCROLL-ANIMATION>=================================
 }
 //=================================
 // function _removeClasses(object, classToRemove) {
